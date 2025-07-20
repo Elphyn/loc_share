9e1880ecd5ab7494dc653f8daef167430e8b6541
Vlad <elphynkarelov@gmail.com>
2025-07-20 17:44:48 +0900

    Start of a project, basic routing

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const multer = require('multer')
+
+const app = express()
+const port = 3000
+app.use(express.static('public'))
+
+const storage = multer.diskStorage({ 
+    destination: (req, file, cb) => {
+        cb(null, 'uploads/')
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname)
+    }
+})
+
+const upload = multer({ storage: storage })
+app.post('/upload', upload.single('myFile'), (req, res) => {
+    console.log(`file: ${req.file}`)
+    res.send("Done")
+})
+
+app.get('/', (req, res) => {
+    res.send("hello")
+    console.log('Hello, World!')
+})
+
+
+
+app.listen(port, '0.0.0.0', () => {
+    console.log(`listening on port ${port}`)
+})
---
+++ notes.md
@@ -0,0 +1,13 @@
+
+
+What should I start with:
+    1. I think first I have to create a server that could recieve files
+
+
+To make a server that recieves files I need:
+    1. Host a node.js server
+    2. Learn POST/GET 
+
+It seems that I should've considered Express first, so I am going to install that first
+Then I would work out how to use POST/GET and attempt sending files to the server
+Also I need to learn how to interact with file system
